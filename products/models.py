96f6ff24e9176bb34f9c66e2b1f464ee9c8e99dd
DerekMor <>
2023-08-07 21:55:25 +0000

    Manually add first set of products through admin panel for design and testing

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(
-        upload_to='../media/product_images/')
+        upload_to='product_images/')
 
     def __str__(self):
         return self.name
