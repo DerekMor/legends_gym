f7f6347dafc61252f8c21b10cc76a78bc6c7ce7b
DerekMor <>
2023-08-08 20:17:38 +0000

    Make search function work and readjust html so error messages show below the header

--- products/views.py
+++ products/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.contrib import messages
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 
 
 def all_products(request):
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'No search term was provided!')
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def single_product(request, pk):
-    
+
     product = get_object_or_404(Product, pk=pk)
 
     context = {
