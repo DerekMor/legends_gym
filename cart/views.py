42336baacff839826a8a4783d0d022103aa860fc
DerekMor <>
2023-08-08 22:59:29 +0000

    add basic unstyled shopping cart page and fuctions

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,22 @@
+from products.models import Product
 from django.shortcuts import render
 
 
 def view_cart(request):
+    cart = request.session.get('cart', {})
+    cart_items = []
 
-    return render(request, 'cart/cart.html')
+    total = 0
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Product, id=product_id)
+        subtotal = product.price * quantity
+        total += subtotal
+        cart_items.append(
+            {'product': product, 'quantity': quantity, 'subtotal': subtotal})
+
+    context = {
+        'cart_items': cart_items,
+        'cart_total': total,
+    }
+
+    return render(request, 'cart/cart.html', context)
