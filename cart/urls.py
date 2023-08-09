373fd52e4ae461b3c7e68653a7b16b868b49a7fe
DerekMor <>
2023-08-09 00:33:51 +0000

    Add udpate quantity to cart and a remove item button than needs to be debugged

--- cart/urls.py
+++ cart/urls.py
@@ -8,5 +8,9 @@ from .views import add_to_cart
 urlpatterns = [
     path('', views.view_cart, name="view_cart"),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:product_id>/',
+         views.remove_from_cart, name='remove_from_cart'),
+    path('update_cart/<int:product_id>/',
+         views.update_cart, name='update_cart'),
 
 ]
--- cart/views.py
+++ cart/views.py
@@ -35,3 +35,32 @@ def add_to_cart(request, product_id):
     messages.success(
         request, f"{quantity} {product.name}{'s' if quantity > 1 else ''} added to your cart.")
     return redirect('products')
+
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    print("Product ID to remove:", product_id)
+    print("Cart before removal:", cart)
+
+    if product_id in cart:
+        del cart[product_id]
+        request.session['cart'] = cart
+        messages.success(request, "Item removed from your cart.")
+    else:
+        messages.error(request, "Item not found in your cart.")
+
+    print("Cart after removal:", cart)
+    return redirect('view_cart')
+
+def update_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        quantity = int(request.POST.get('quantity', 1))
+
+        if quantity > 0:
+            cart = request.session.get('cart', {})
+            cart[product_id] = quantity
+            request.session['cart'] = cart
+
+    return redirect('view_cart')
