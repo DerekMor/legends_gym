f2dd4019566b0ea207ea36dcf53564d27caec50f
DerekMor <>
2023-08-10 20:38:56 +0000

    Fixed bug not removing items from cart(datatype missmatch needed cast to string)

--- cart/views.py
+++ cart/views.py
@@ -7,18 +7,18 @@ def view_cart(request):
     cart = request.session.get('cart', {})
     cart_items = []
 
-    cart_total = 0  # Initialize cart_total
+    cart_total = 0
 
     for product_id, quantity in cart.items():
         product = get_object_or_404(Product, id=product_id)
         subtotal = product.price * quantity
-        cart_total += subtotal  # Add subtotal to cart_total
+        cart_total += subtotal
         cart_items.append(
             {'product': product, 'quantity': quantity, 'subtotal': subtotal})
 
     context = {
         'cart_items': cart_items,
-        'cart_total': cart_total,  # Pass cart_total to the context
+        'cart_total': cart_total,
     }
 
     return render(request, 'cart/cart.html', context)
@@ -31,7 +31,7 @@ def add_to_cart(request, product_id):
     cart = request.session.get('cart', {})
     cart[product_id] = cart.get(product_id, 0) + quantity
     request.session['cart'] = cart
-
+    print('debug. ID:', product_id)
     messages.success(
         request, f"{quantity} {product.name}{'s' if quantity > 1 else ''} added to your cart.")
     return redirect('products')
@@ -39,18 +39,20 @@ def add_to_cart(request, product_id):
 
 def remove_from_cart(request, product_id):
     cart = request.session.get('cart', {})
-    print("Product ID to remove:", product_id)
-    print("Cart before removal:", cart)
+    str_product_id = str(product_id)
 
-    if product_id in cart:
-        del cart[product_id]
+    if str_product_id in cart:
+        print('debug. if block:', cart)
+        del cart[str_product_id]
         request.session['cart'] = cart
+        request.session.save()
         messages.success(request, "Item removed from your cart.")
     else:
+        print('debug. else block:', cart)
         messages.error(request, "Item not found in your cart.")
 
-    print("Cart after removal:", cart)
     return redirect('view_cart')
+
 
 def update_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
