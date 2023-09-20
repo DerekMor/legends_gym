69adebf42bae2ebbc9b07f85e2ecb733a5702f76
DerekMor <derekmoriarty24@gmail.com>
2023-09-20 22:07:28 +0000

    Add wishlist model to products app

--- products/models.py
+++ products/models.py
@@ -25,3 +25,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
