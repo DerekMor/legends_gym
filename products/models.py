f0a1bf3f29c1150a9b97fac88c770327cbe70a6f
DerekMor <derekmoriarty24@gmail.com>
2023-09-20 22:15:58 +0000

    Add review model to products app

--- products/models.py
+++ products/models.py
@@ -32,3 +32,15 @@ class Wishlist(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Wishlist"
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    rating = models.IntegerField(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.product.name}"
