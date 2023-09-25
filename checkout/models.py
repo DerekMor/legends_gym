9fdf09cd65be5a0ddd80f0388489422d007913ee
DerekMor <derekmoriarty24@gmail.com>
2023-09-25 22:19:32 +0000

    Add discount code model and logic to apply to order model

--- checkout/models.py
+++ checkout/models.py
@@ -21,6 +21,7 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
+    discount_code = models.ForeignKey(DiscountCode, on_delete=models.SET_NULL, blank=True, null=True)
 
     def _generate_order_number(self):
 
@@ -34,6 +35,18 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+
+    def apply_discount_code(self, discount_code):
+        try:
+            code = DiscountCode.objects.get(code=discount_code, is_active=True, used=False)
+            discount_amount = (code.percentage / 100) * self.order_total
+            self.order_total -= discount_amount
+            
+            code.used = True
+            code.save()
+            self.save()
+        except DiscountCode.DoesNotExist:
+            raise ValidationError("Invalid or used discount code.")
 
 
 class OrderLineItem(models.Model):
@@ -59,3 +72,12 @@ class OrderHistory(models.Model):
     
     def __str__(self):
         return f"{self.user.username}'s Order History"
+
+class DiscountCode(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    used = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.code
