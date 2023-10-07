ec634c388790ce32ce5b3e99a9dac28f866627c2
DerekMor <derekmoriarty24@gmail.com>
2023-10-07 15:23:22 +0000

    Add order history section to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from .models import Customer
 from .forms import UserProfileForm, ProfilePictureForm
 from django.contrib.auth.decorators import login_required
+from checkout.models import Order
 
 # Create your views here.
 @login_required
 def profile(request):
     user_profile = Customer.objects.get(user=request.user)
 
+    orders = Order.objects.filter(customer=user_profile).order_by('-date')
+    
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=user_profile)
         profile_picture_form = ProfilePictureForm(request.POST, request.FILES, instance=user_profile)
@@ -19,4 +22,4 @@ def profile(request):
         form = UserProfileForm(instance=user_profile)
         profile_picture_form = ProfilePictureForm(instance=user_profile)
 
-    return render(request, 'profile.html', {'form': form, 'profile_picture_form': profile_picture_form})
+    return render(request, 'profile.html', {'form': form, 'profile_picture_form': profile_picture_form, 'orders': orders})
